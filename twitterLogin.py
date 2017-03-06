e899a8a4b7a54174c946b026cc81b8060d2d7ef9
Fady Machaalani <fmachaalani@gmail.com>
2017-03-06 13:57:29 -0700

    Update twitterLogin.py

--- twitterLogin.py
+++ twitterLogin.py
@@ -1,21 +1,21 @@
-def main():
+def main(connection):
 	input_valid = False
 	while (not input_valid):
+		print("Welcome to Twooter")
 		choice = input("Login(l) or Register(r)? ")
 		if choice.strip().lower() == 'l' or choice.strip().lower() == "login":
-			print("login")
+			login(connection)
 			input_valid = True
 		elif choice.strip().lower() == 'r' or choice.strip().lower() == "register":
 			#Call register function here. 
-			print("reg")
+			print("This is the register option, delete this line when added a proper function")
 			input_valid = True
 		else:
 			print("Please choose an option")
 
 
-def login():
-	pass
-
-
-
-
+def login(connection):
+	cursor = connection.cursor()
+	cursor.execute("SELECT usr, pwd FROM users")
+	rows = cursor.fetchall()
+	print(rows)
