7eda5e3c09aae6f9445cfede0ec386aed70c41bf
mattr95 <matthew@Matthews-MacBook-Pro.local>
2017-03-06 13:52:05 -0700

    Update registration functions

--- registration.py
+++ registration.py
@@ -6,14 +6,14 @@ import re
 def registration(connection):
 
 	def __init__(self):
-		print('\n'+"User Registration"+'\n')
+		print('\n'+"Twooter User Registration")
 		print("-------------------------------------------------------------")
 		
 		getUsr(connection)
 
 
 def getUsr(connection):
-	curs = connection.cursor()
+	cursor = connection.cursor()
 
 	name = input("Name: ")
 	while len(name) > 20:
@@ -22,3 +22,25 @@ def getUsr(connection):
 	email = input("Email: ")
 	while re.match(".*@.*\..*", email) == None:
 		email = input("Invalid email format!"+'\n'+"Email: ")
+	while len(email) > 15:
+		email = input("Email must be less than 15 characters!"+'\n'+"Email: ")
+
+	city = input("City: ")
+	while len(city) > 12:
+		city = input("City must be less than 12 characters!"+'\n'+"Email: ")
+
+	timezone = float(input("Timezone: "))
+	while (timezone < -12) or (timezone > 14):
+		timezone = float(input("Invalid timezone!"+'\n'+"Timezone: "))
+
+	print("Enter a password (Max 4 characters)")
+	password = gp.getpass()
+	while len(password) > 4:
+		print("Password must be less than 4 characters!")
+		password = getpass()
+
+	# query to get max user id
+	createUsr(connection)
+
+def createUsr(connection):
+	
