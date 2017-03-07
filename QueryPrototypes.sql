7b6fa1cde6e7f6c6d40cb1066df61e69bfa3f7b4
aridgway120 <aridgway@ualberta.ca>
2017-03-07 13:50:41 -0700

    .

---
+++ QueryPrototypes.sql
@@ -0,0 +1,11 @@
+-- HOMEPAGE:        all tweets and retweets from followed users; ordered descending by date
+
+-- TWOOT SEARCH:    all tweets with keyword k as a hashtag or including k; order by date descending
+
+-- USER SEARCH:     first, users whose names match keyword k, ordered by name len ascending
+--                  then, users whose cities match k, ordered by city len ascending
+
+-- COMPOSE TWOOT:   save words preceded by a # in hashtags and mentions
+--  this will largely be on the python side
+
+-- LIST FOLLOWERS:
