7dafeea04d9f387d5ddc8bac4158297404a19612
aridgway120 <aridgway@ualberta.ca>
2017-03-09 15:08:43 -0700

    Revert "Formatted feed"
    This reverts commit fc74c4c2096a2a1823d6f0b7c1cea79825096a3a.

--- feed.py
+++ feed.py
@@ -1,6 +1,6 @@
 # initial feed; pages of 5 tweets, option of more info.
 import cx_Oracle as cx
-import textwrap, os
+import textwrap
 
 def feed(usr, connection):
 
@@ -20,43 +20,11 @@ def feed(usr, connection):
 
 	cursor = connection.cursor()
 	cursor.execute(query, {'usr':usr})
-	system_message = ""
 
-	twootdata = cursor.fetchmany(numRows = 5)
-	while True:
-		display(twootdata, 5)
-		print('\n'+system_message)
-		system_message = ""
-
-		userin = input("Selection:").lower()
-		if userin=='next':
-			newdata = cursor.fetchmany(numRows = 5)
-			if newdata == []:
-				display(twootdata, 5)
-				system_message = "No more twoots to display."
-			else:
-				twootdata = newdata
-		elif userin=='top':
-			cursor.execute(query, {'usr':usr})
-			twootdata = cursor.fetchmany(numRows = 5)
-		elif userin=='exit':
-			return
-		else:
-			display(twootdata, 5)
-			system_message = "INVALID SELECTION: see Navigation above."
-
-
-	#twootdata = cursor.fetchmany(numRows = 5)
-	#display(twootdata, 5)
+	twootdata = cursor.fetchmany(numRows = 100)
 
 	# each twoot consists of tid, author, date, text, replyto, and rt
 	#						  0		1		2	  3		4			5
-
-def display(twootdata, n):
-	# twootdata = list of tuples formatted as (tid, author, date, text, replyto, and rt)
-	#										   int	string	date  string string		 int (1 or 0)
-	# n = number of items per page
-	os.system("clear")
 
 	for twoot in twootdata:
 		print('|' + "-"*55)
@@ -73,7 +41,4 @@ def display(twootdata, n):
 
 	print('|' + "-"*55)
 	print('/'*56)
-	print("Navigation\n"\
-		"next:\tDisplay "+str(n)+" more Twoots.\n"\
-		"top: \tReturn to top of feed.\n"
-		"exit:\tClose Twooter.")
+	print("Navigation")
