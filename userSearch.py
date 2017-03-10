38e8dfc2a0ae2c3794030d5023006a0e852f60bd
Fady Machaalani <fmachaal@cs.ualberta.ca>
2017-03-09 22:57:43 -0700

    Need to add 5 search per

--- userSearch.py
+++ userSearch.py
@@ -8,27 +8,52 @@ import followers
 def search(usr, connection):
 	keywords = getKeywords()
 	cursor = connection.cursor()
-	query = buildQuery(keywords)
-	rows = cursor.execute(query).fetchall()
-	rows.sort(key = lambda x : len(x[1].strip()))
-	followers.followers(usr, connection, rows)
+	nameQuery = buildNameQuery(keywords)
+	cityQuery = buildCityQuery(keywords)
+	if(nameQuery != "" and cityQuery != ""):
+		names = cursor.execute(nameQuery).fetchall()
+		names.sort(key = lambda x : len(x[1].strip()))
+		cities = cursor.execute(cityQuery).fetchall()
+		cities.sort(key = lambda t: len(t[2].strip()))
+
+		people = names + cities
+
+		followers.followers(usr, connection, people)
 
 
 
-def buildQuery(keywords):
+def buildNameQuery(keywords):
 	if len(keywords) > 0:
-			whereClause1 = "WHERE ((LOWER(u.name) LIKE "
-			i = 0
-			for word in keywords:
-				if i == 0:
-					whereClause1 += "'%"+word+"%')"
-					i += 1
-				else:
-					whereClause1 += " OR (LOWER(name) LIKE '%"+word+"%')"
-			whereClause1 += ")"
-
-	query = "SELECT u.usr, u.name FROM users u " +whereClause1
+		whereClause1 = "WHERE ((LOWER(u.name) LIKE "
+		i = 0
+		for word in keywords:
+			if i == 0:
+				whereClause1 += "'%"+word+"%')"
+				i += 1
+			else:
+				whereClause1 += " OR (LOWER(u.name) LIKE '%"+word+"%')"
+		whereClause1 += ")"
+		query = "SELECT u.usr, u.name FROM users u " +whereClause1
+	else:
+		query = ""
 	return query
+
+
+def buildCityQuery(keywords):
+	if len(keywords) > 0:
+		whereClause1 = "WHERE ((LOWER(u.city) LIKE "
+		i = 0
+		for word in keywords:
+			if i == 0:
+				whereClause1 += "'%"+word+"%')) MINUS (SELECT u.usr, u.name, u.city from users u WHERE ((LOWER(u.name) LIKE '%"+word+"%')))"
+				i += 1
+
+		query = "SELECT u.usr, u.name, u.city FROM users u " +whereClause1
+	else:
+		query = ""
+	return query
+
+
 
 
 def getKeywords():
