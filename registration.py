524e26f7dd036117ce8132cdc1c089dedf34aa56
mattr95 <matthew@vpn0185.remote.ualberta.ca>
2017-03-11 22:58:20 -0700

    .

--- registration.py
+++ registration.py
@@ -12,7 +12,7 @@ def getUsr(connection):
 	cursor = connection.cursor()
 
 	# Get new user info
-	name = input("Name: ").lower().strip()
+	name = input("Name: ").strip()
 	while len(name) > 20:
 		name = input("Name must be less than 20 characters!"+'\n'+"Name: ").lower().strip()
 
